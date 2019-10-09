b9103bfd3596c6511007748aa57e3f5f6d1b5746
cgr <cgr@pc1011601230.uni-regensburg.de>
2019-10-09 11:24:44 +0200

    update manuscript and lit

---
+++ R/GlobalTest.R
@@ -0,0 +1,24 @@
+#' GlobalTest
+#' 
+#' Test versus global alternative in Functional Linear Regression
+#'
+#' 
+#' @param obj A fitted model object, see also estFLR
+#' 
+#' @param alternative The global alternative to test against. The alternative
+#' can be specified as a vector holding the discretized function object or,
+#' alternatively, as an expansion of a given basis system. Defaults to the zero function.
+#'
+#' @param type Which test to use. Currently, the following alternatives are available:
+#' 
+#' @export
+GlobalTest <- function(obj, alternative, type,...){
+
+    if (!is.identical(class(obj), "FLR")){
+        stop("obj must be of class FLR!")
+    }
+
+    
+    
+    
+}
