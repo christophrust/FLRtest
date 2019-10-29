d8e53ebe1a04e2c858b2cce3f39e432c99aece8e
Christoph Rust <christoph.g.rust@gmail.com>
2019-10-29 11:24:38 +0100

    adjust tests to current function definitions

--- tests/testthat/test_FLR.R
+++ tests/testthat/test_FLR.R
@@ -4,13 +4,15 @@ library(splines)
 ## simulate some data
 set.seed(123)
 p <- 100
-N <- 300
+N <- 1000
 grd <- seq(0,1,length = p)
 X <- t(bs(x= grd, df = p, intercept = TRUE) %*% matrix(rnorm(N*p), ncol = N))
 beta <- sin(10*grd)
 y <- X %*% beta/p + rnorm(N, sd=0.1)
-res <- EstFLM(y, X, model = list(type = "smoothspline", rho = 0.00000001), intercept = FALSE)
+res <- EstFLM(y, X, type = "smoothspline", rho = 0.00000001, intercept = TRUE)
+res1 <- EstFLM(y, X, type = "fpc", cpv = 0.99, intercept = TRUE)
 
+matplot(y = cbind(beta, res$coefficient$beta,res1$coefficient$beta), x = grd, t = "l")
 
 
 
@@ -19,7 +21,7 @@ context("Functional Regression - Error messages")
 
 test_that("EstFLM", {
     expect_error(EstFLM())
-    expect_error(EstFLM(2,5,model = list(type = "abc",rho = 3)))
+    expect_error(EstFLM(2,5,type = "abc",rho = 3))
 })
 
 
@@ -29,12 +31,20 @@ context("Functional Regression - Estimation")
 test_that("Estimation", {
 
     ## estimate, given rho
-    res1 <- EstFLM(y, X, model = list(type = "smoothspline", rho = 0.00000001), intercept = TRUE)
+    res1 <- EstFLM(y, X, type = "smoothspline", rho = 0.00000001, intercept = TRUE)
     expect_equal(mean(res1$residuals), 0)
 
     ## estimate, given df
-    res2 <- EstFLM(y, X, model = list(type = "smoothspline", df = 6), intercept = TRUE)
+    res2 <- EstFLM(y, X, type = "smoothspline", df = 6, intercept = TRUE)
     expect_equal(mean(res2$residuals), 0)
+
+    ## fpca estimator
+    res3 <- EstFLM(y, X, type = "fpc", df = 6, intercept = TRUE)
+    expect_equal(mean(res3$residuals), 0)
+
+    res4 <- EstFLM(y, X, type = "fpc", cpv = 0.9, intercept = TRUE)
+    expect_equal(mean(res3$residuals), 0)
+    
     
 })
 
@@ -48,6 +58,6 @@ test_that("Global Test: error messages",{
 
 test_that("Global Test: plausible values",{
     expect_true(GlobalTest(res)$pval < 0.6)
-    expect_true(GlobalTest(res, alternative = beta)$pval>0.0001)
+    expect_true(GlobalTest(res, null = beta)$pval>0.0001)
 })
 
