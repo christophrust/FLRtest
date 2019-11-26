c58577fb732ea106fa452573229f7612eb8c374d
christophrust <christoph.g.rust@googlemail.com>
2019-11-26 11:32:23 +0100

    add function dfGivenRho to compute df from full model for given (log) rho

---
+++ src/rho2df.c
@@ -0,0 +1,101 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <R_ext/Lapack.h>
+#include "flrtest.h"
+
+
+
+
+
+/* 
+
+Function to compute effective degrees of freedom of the smspl 
+fit given a smoothing paramter rho 
+
+*/
+double dfGivenRho(double x, double *npXtX, double *X, double *Amat, int Nobs, int dim){
+  
+  
+  
+  int i,j;
+  int ll, ur;
+  
+  double alpha = 1/((double) Nobs);
+  double beta = 0, res = 0;
+  
+  int info;
+  
+  // initialize arrays
+  double *npXtXplusA, *npXtXplusA1Xt;
+  npXtXplusA = (double *) malloc(pow(dim,2) * sizeof(double));
+  npXtXplusA1Xt = (double *) malloc( dim*Nobs  * sizeof(double));;
+  
+  
+  // sum of npXtX + rho * A
+  for (i=0; i< (dim * dim); i++){
+    npXtXplusA[i] = npXtX[i] + exp(x) * Amat[i];
+  }
+  
+  // compute cholesky
+  F77_CALL(dpotrf)("U",&dim, npXtXplusA, &dim, &info);
+  
+  // compute inverse from cholesky
+  if (info){
+    error("Error when using cholesky decomposition, Info = %i!", info);
+  } else {
+    
+    F77_CALL(dpotri)("U",&dim, npXtXplusA, &dim, &info);
+  }
+  
+  // compute product of inv(XtX + rho * A) * X'
+  if (info){
+    
+    error("Error when inverting after cholesky!");
+  } else {
+    
+    // make matrix symmetric
+    for (j = 0; j<dim; j++){
+      for (i = j+1; i<dim; i++){
+	ll = j*dim + i;
+	ur = i*dim + j;
+	npXtXplusA[ll] = npXtXplusA[ur];
+      }
+    }
+    
+    F77_CALL(dgemm)("n","t",&dim, &Nobs, &dim, &alpha, npXtXplusA, &dim,
+		    X, &Nobs, &beta, npXtXplusA1Xt, &dim); 
+  }
+  
+  // compute trace of X * npXtXplusA1Xt
+  for (int i=0; i<Nobs; i++){
+    for (int j=0; j<dim; j++){
+      res += X[j*Nobs + i] * npXtXplusA1Xt[i * dim + j];
+    }
+  }
+  
+  res *= (double) 1/dim;
+  
+  return res;
+}
+
+
+
+/* R wrapper around the above function */
+SEXP R_dfGivenRho(SEXP rho, SEXP npXtX, SEXP X, SEXP Amat, SEXP Nobs, SEXP dim){
+
+  SEXP res = PROTECT(allocVector(REALSXP, 1));
+
+  //double df = 0;
+  
+  
+  REAL(res)[0] = dfGivenRho(REAL(rho)[0],
+		      REAL(npXtX),
+		      REAL(X),
+		      REAL(Amat),
+		      *INTEGER(Nobs),
+		      *INTEGER(dim));
+  
+  //REAL(res)[0] = df;
+  UNPROTECT(1);
+  return res;
+}
