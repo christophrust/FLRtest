f5e3755fca2b0b79c3b2971242f954ea163eb56c
christophrust <christoph.g.rust@googlemail.com>
2019-11-27 21:46:57 +0100

    add to print.flm.test() also directed test

--- R/s3methods.R
+++ R/s3methods.R
@@ -1,16 +1,30 @@
 ## print method for test object
 #' @export
 print.flm.test <- function(x,...){
-    cat("Test of association in the functional linear model\n")
-    cat("--------------------------------------------------\n")
-    cat(sprintf("Type: %s-test\n",x$type))
-    cat(sprintf("Null hypothesis: %s\n",
-                if (is.null(x$alternative))
-                    "no effect" else "specified alternative"))
-    cat(sprintf("Test statistic: %.2f at %.2f and %.2f degrees of freedom\n",
-                x$statistic, x$df[1], x$df[2]))
-    cat(sprintf("RSS0 = %f; RSS1 = %f\n", x$RSS0, x$RSS1))
-    cat(sprintf("P-value: %f\n",x$pval))    
+
+    if (attr(x, which = "test.type") == "global"){
+        cat("Test of association in the functional linear model\n")
+        cat("--------------------------------------------------\n")
+        cat(sprintf("Type: %s-test\n",x$type))
+        cat(sprintf("Null hypothesis: %s\n",
+                    if (is.null(x$alternative))
+                        "no effect" else "specified alternative"))
+        cat(sprintf("Test statistic: %.2f at %.2f and %.2f degrees of freedom\n",
+                    x$statistic, x$df[1], x$df[2]))
+        cat(sprintf("RSS0 = %f; RSS1 = %f\n", x$RSS0, x$RSS1))
+        cat(sprintf("P-value: %f\n",x$pval))
+
+    } else  if (attr(x, which = "test.type") == "directed"){
+
+        pm <- max(which(x$rej))
+        
+        cat("Directed test of association in the functional linear model\n")
+        cat("-----------------------------------------------------------\n")
+        cat(sprintf(
+            "Significant influence up to grid index %i.\nThis corresponds to %.2f in normalized domain\n", pm, seq(0,1, len = length(x$rej +1))[pm]
+))
+        cat(sprintf("Significance level used: %.2f\n", 1-x$conf.level))
+    }
 }
 
 
