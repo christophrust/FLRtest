5c9eff093ebcdc8f99297df9fd6a7fab50e7f796
christophrust <christoph.g.rust@googlemail.com>
2020-04-22 16:12:14 +0200

    add splitspline unit test

--- tests/testthat/test_splines.R
+++ tests/testthat/test_splines.R
@@ -15,4 +15,33 @@ test_that("Spline Basis Functions",{
                                     x = seq(0,1, len = 10),
                                     derivs = 0L)
     expect_equal(basis1, basis2)
+
+    knots <- c(rep(0,time = 4), sort(runif(10)), rep(1,time = 4))
+    xvals <- runif(1000)
+    basis1 <- .Call("R_spline_basis",
+                    knots = knots,
+                    order = 4L,
+                    xvals = xvals,
+                    derivs = 0L)
+    basis2 <- splines::splineDesign(knots = knots,
+                                    ord = 4L,
+                                    x = xvals,
+                                    derivs = 0L)
+    expect_equal(basis1, basis2)
+})
+
+
+test_that("SplitSpline",{
+
+    grd <- seq(0,1, len = 100)
+    knots <- sort(c(rep(0,times = 3), rep(1, times = 3),
+                    seq(0,1, by = 0.1), rep(0.5, times = 3)))
+    a <- .Call("R_SplitSplineBasis",
+          grd = grd,
+          df = 16L,
+          splitpoint = 0.5)
+    b <- splineDesign(knots = knots, x = grd)
+    matplot(x = grd, y = a, type = "l")
+
+    expect_equal(a, b)
 })
