c36094b9cb414225f3e7b15c9e623a0fa90693e2
christophrust <christoph.g.rust@googlemail.com>
2020-04-29 18:40:45 +0200

    matrix multiplication from blas and call to crossprod added and tested

--- src/estmodel_spl.c
+++ src/estmodel_spl.c
@@ -1,7 +1,5 @@
-#include <R.h>
-#include <Rinternals.h>
-#include <R_ext/Lapack.h>
 #include "flrtest.h"
+
 /*
   estmodel computes RSS, effective degrees of freedom or coefficient function
   for a smoothing spline estimation of a functional linear model. Via a selector,
@@ -38,15 +36,94 @@
 
 double * estmodel_spl(struct callinfo_spl *model, int retbeta){
 
-  /* return array */
+  /* array containing result of estimation */
   double *res;
+  res = (double *) R_alloc(1, sizeof(double));
+  res[0] = 2.0;
 
   /* allocate space for necessary intermediate results */
   double *pXB;   // 1/p * X * Basis
-  pXB = (double *) R_alloc( *model->n * *model->p, sizeof(double));
+  pXB = (double *) R_alloc( *model->n * *model->dim, sizeof(double));
+
+  /* this in fact will hold pXB'pXB */
+  double *XtX;
+  XtX = (double *) R_alloc( *model->dim * *model->dim, sizeof(double));
+
+  /* compute pXB  (1/p * X %*% B)*/
+  //double alpha = 1.0;
+  double alpha = 1/(double) *model->p;
+  double beta = 0.0;
+
+  Rprintf("%i;--\n", (*model->n * *model->dim));
+
+  F77_CALL(dgemm)("n", "n", model->n, model->dim, model->p, &alpha,
+                  model->X, model->n, model->Basis, model->p, &beta,
+                  pXB, model->n);
+
+  /* compute matrix product by hand: */
+  /* double temp = 0.0; */
+  /* for (int i=0; i < *model->n; i++){ */
+  /*   for (int j = 0; j < *model->dim; j++){ */
+  /*     temp = 0.0; */
+  /*     for (int k =0; k < *model->p; k++){ */
+  /*       temp += model->X[i + k* *model->n] * model->Basis[j * *model->p + k]; */
+  /*     } */
+  /*     pXB1[i + j * *model->dim ] = temp; */
+  /*   } */
+  /* } */
+
+  /* for (int i=0; i < 10; i++){ */
+  /*   Rprintf("%f, ",model->Basis[i]); */
+
+  /* } */
+  /* Rprintf("\n--------\n"); */
+
+
+  /* for (int i=0; i < 10; i++){ */
+  /*   Rprintf("%f, ",model->X[i]); */
+  /* } */
+  /* Rprintf("\n--------\n"); */
+
+
+  /* for (int i=0; i < 10; i++){ */
+  /*   Rprintf("%f, ",pXB[i]); */
+  /* } */
+  /* Rprintf("\n-----a---\n"); */
+
+
+  /* compute XtX*/
+  internal_crossprod(pXB, *model->n, *model->dim, pXB, *model->n, *model->dim, XtX);
+
+  /* for (int i=0; i < 10; i++) { */
+  /*   Rprintf("%f; ", XtX[i]); */
+  /* } */
+
+
+  return res;
+}
 
 
 
+
+SEXP R_estmodel_spl(SEXP y, SEXP X, SEXP basis, SEXP n, SEXP p, SEXP dim) {
+
+  struct callinfo_spl model;
+
+  model.X = REAL(X);
+  model.y = REAL(y);
+  model.Basis = REAL(basis);
+  model.n = INTEGER(n);
+  model.p = INTEGER(p);
+  model.dim = INTEGER(dim);
+
+  double *val;
+  val = estmodel_spl(&model, 0);
+
+  SEXP res;
+  res = PROTECT(allocVector(REALSXP, 1));
+  UNPROTECT(1);
+
+  REAL(res)[0] = val[0];
 
   return res;
 }
