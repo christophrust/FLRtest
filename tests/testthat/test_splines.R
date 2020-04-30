b15dd11a9aa8ba55cc28d379ae5be2dc527e3a49
christophrust <christoph.g.rust@googlemail.com>
2020-04-30 13:23:36 +0200

    add unit tests for estmodel_spl

--- tests/testthat/test_splines.R
+++ tests/testthat/test_splines.R
@@ -44,4 +44,48 @@ test_that("SplitSpline",{
     matplot(x = grd, y = a, type = "l")
 
     expect_equal(a, b)
+
+})
+
+
+test_that("Spline Model Estimation",{
+
+    if (0) {
+        devtools::load_all()
+    }
+
+    basis <- .Call("R_SplitSplineBasis",
+                   grd = seq(0,1,len = 100),
+                   df = 10L,
+                   splitpoint = 0.5)
+
+
+    y <- rnorm(1000)
+    X <- matrix(rnorm(1e5), ncol=100)
+
+    pXB <- 1/dim(X)[2] * X %*% basis
+
+    betahat1 <- .Call("R_estmodel_spl",
+                     y = y,
+                     X = X,
+                     basis = basis,
+                     n = 1000L,
+                     p = 100L,
+                     dim = 10L,
+                     retbeta = 1L)
+
+    betahat2 <- as.vector(basis %*% (solve(crossprod(pXB)) %*% t(pXB)) %*% y)
+
+    expect_equal(betahat1, betahat2)
+
+    rss <- .Call("R_estmodel_spl",
+                 y = y,
+                 X = X,
+                 basis = basis,
+                 n = 1000L,
+                 p = 100L,
+                 dim = 10L,
+                 retbeta = 0L)[2]
+
+    expect_equal(rss, sum((y - pXB %*% (solve(crossprod(pXB)) %*% t(pXB)) %*% y)^2))
 })
