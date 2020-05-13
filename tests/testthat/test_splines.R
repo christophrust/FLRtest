a89ec977d0689413446a582ca90927f4835a5b3c
christophrust <christoph.g.rust@googlemail.com>
2020-05-13 11:04:56 +0200

    change unit tests for full model estimation

--- tests/testthat/test_splines.R
+++ tests/testthat/test_splines.R
@@ -53,42 +53,45 @@ test_that("SplitSpline - selector", {
 
     grd <- seq(0,1, by = 0.05)
 
-    for (i in 1:100){
+    for (splitpoint in grd){
 
-        (splitpoint <- runif(1, min = 0.1, max = 0.9))
+        ##(splitpoint <- runif(1, min = 0.1, max = 0.9))
         ## knots <- sort(c(rep(0,times = 3), rep(1, times = 3),
         ##                 seq(0,1, by = 0.1), rep(splitpoint, times = 3)))
 
-
+        splitpoint <- grd[21]
         a <- .Call("R_SplitSplineBasis",
                    grd = grd,
                    df = 16L,
                    splitpoint = splitpoint)
 
+        solve(t(a$basis) %*% a$basis)
+
         b <- splineDesign(knots = a$knots, x = grd)
         expect_equal(sum(abs(a$basis-b)), 0)
 
-        expect_equal(sum(a$basis[grd > splitpoint, 1:(a$selector)]), 0)
-        expect_equal(sum(a$basis[grd <= splitpoint, (a$selector+1):16]), 0)
+        expect_equal(sum(a$basis[grd >= splitpoint, 1:(a$selector)]), 0)
+        expect_equal(sum(a$basis[grd < splitpoint, (a$selector+1):16]), 0)
     }
 
 })
 
+set.seed(1234)
+y <- rnorm(1000)
+X <- matrix(rnorm(1e5), ncol=100)
+SplitBasis <- .Call("R_SplitSplineBasis",
+               grd = seq(0,1,len = 100),
+               df = 10L,
+               splitpoint = 0.5)
 
-test_that("Spline Model Estimation",{
+test_that("Spline Model Estimation -- Full model",{
 
     if (0) {
         devtools::load_all()
     }
 
-    basis <- .Call("R_SplitSplineBasis",
-                   grd = seq(0,1,len = 100),
-                   df = 10L,
-                   splitpoint = 0.5)$basis
 
-
-    y <- rnorm(1000)
-    X <- matrix(rnorm(1e5), ncol=100)
+    basis <- SplitBasis$basis
 
     pXB <- 1/dim(X)[2] * X %*% basis
 
@@ -99,6 +102,7 @@ test_that("Spline Model Estimation",{
                      n = 1000L,
                      p = 100L,
                      dim = 10L,
+                     selector = 10L,
                      retbeta = 1L)
 
     betahat2 <- as.vector(basis %*% (solve(crossprod(pXB)) %*% t(pXB)) %*% y)
