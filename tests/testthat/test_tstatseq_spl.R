0647f3024a6e462e409163ef031fce6decce8aa7
christophrust <christoph.g.rust@googlemail.com>
2020-05-19 10:47:32 +0200

    adjust unit test for tstatseq_spl function (use test size)

--- tests/testthat/test_tstatseq_spl.R
+++ tests/testthat/test_tstatseq_spl.R
@@ -2,7 +2,7 @@ if (0) {
     devtools::load_all()
 }
 
-set.seed(1234)
+##set.seed(1234)
 k <- 10L
 p <- 100L
 N <- 1000L
@@ -20,14 +20,8 @@ BasisAndSelectors <- lapply(grd[-p], function(splitpt){
           splitpoint = splitpt)
 })
 
-a <- .Call("R_SplitSplineBasis",
-      grd = grd,
-      df = k ,
-      splitpoint = grd[99])
-
-
 Basis <- vapply(BasisAndSelectors, function(x) x$basis, matrix(0, nrow = p, ncol = k))
-
+Selectors <- vapply(BasisAndSelectors, function(x) x$selector, 0)
 ## check that all basis matrices are full-rank:
 fullrank <- vapply(BasisAndSelectors, function(x) abs(det(crossprod(x$basis)))>0, TRUE)
 
@@ -35,15 +29,37 @@ context("tstatseq_spl")
 
 test_that("simple call tstatseq_spl",{
 
-    .Call("tstatseq_spl",
-          y = y,
-          X = X,
-          Basis = as.vector(Basis),
-          selectors = as.integer(Selectors),
-          p = p,
-          n = N,
-          df = as.integer(k + intercept),
-          intercept = intercept)
+    intercept = 0L
+    a <- .Call("tstatseq_spl",
+               y = y,
+               X = X,
+               Basis = as.vector(Basis),
+               selectors = as.integer(Selectors),
+               p = p,
+               n = N,
+               df = as.integer(k + intercept),
+               intercept = intercept)
 
+    intercept = 1L
+    b <- .Call("tstatseq_spl",
+               y = y,
+               X = X,
+               Basis = as.vector(Basis),
+               selectors = as.integer(Selectors),
+               p = p,
+               n = N,
+               df = as.integer(k + intercept),
+               intercept = intercept)
+
+    a_pvals <- pf(a[,5], df1 = a[,1] - a[,3],
+                  df2 = N - a[,1], lower.tail = FALSE)
+    b_pvals <- pf(b[,5], df1 = b[,1] - b[,3],
+                  df2 = N - b[,1], lower.tail = FALSE)
+
+
+    expect_true(mean(a_pvals < 0.01)<=0.01)
+    expect_true(mean(b_pvals < 0.01)<=0.01)
 
 })
+
+
