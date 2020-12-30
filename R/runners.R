895354fab3d87ed8bccd37f0db9f9ad352754479
christophrust <christoph.g.rust@googlemail.com>
2020-12-30 21:19:57 +0100

    add data frame to package

---
+++ R/runners.R
@@ -0,0 +1,16 @@
+#' Torque curves and strike index of recreational barefootrunners.
+#'
+#' A data frame containing the necessary data to replicate the results of
+#' the manuscript "Directed Local Testing in the Functional Linear Model"
+#' containing two objects:
+#'
+#' \itemize{
+#'  \item Y_si_bare. vector containing the strike index of individual runners
+#'  \item X_curves_bare. matrix holding the discretizes observations of the torque
+#'           curves. Rows correspond to grid values and columns to observations.
+#' }
+#'
+#' @name runners
+#' @docType data
+#' @usage data(runners)
+NULL
